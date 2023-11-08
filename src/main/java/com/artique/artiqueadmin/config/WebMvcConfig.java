ba52b455390625bd4f89cc91f6a9c5f7dab8b868
johan1103 <johan1103@naver.com>
2023-11-09 03:35:08 +0900

    [Feat] add converter

--- src/main/java/com/artique/artiqueadmin/config/WebMvcConfig.java
+++ src/main/java/com/artique/artiqueadmin/config/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package com.artique.artiqueadmin.config;
 
+import com.artique.artiqueadmin.converter.ReportTypeConverter;
+import com.artique.artiqueadmin.entity.ReportType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -8,8 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
-
+  private final ReportTypeConverter reportTypeConverter;
   @Override
   public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(reportTypeConverter);
   }
 }
